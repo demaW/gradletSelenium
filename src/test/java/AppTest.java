c97226bdac2bea0d6f7188dfcd12a9df776867ee
Volodymyr Demchyk <demavov@gmail.com>
2018-01-11 01:05:05 +0200

    Initial commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+    @Test
+    public void someTest(){
+        System.out.println("In test ...");
+        Assert.assertTrue(true);
+    }
+}
---
+++ src/test/java/AppTest2.java
@@ -0,0 +1,10 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest2 {
+    @Test
+    public void test2() {
+        System.out.println("in test 2");
+        Assert.assertFalse(true);
+    }
+}
